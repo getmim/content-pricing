6731e7958090d3021289ec5ed65371d48cc864ea
Iqbal Fauzi <iqbalfawz@gmail.com>
2020-02-16 22:43:57 +0700

    Add model method to get unpriced content

--- modules/content-pricing/config.php
+++ modules/content-pricing/config.php
@@ -2,7 +2,7 @@
 
 return [
     '__name' => 'content-pricing',
-    '__version' => '0.0.2',
+    '__version' => '0.1.0',
     '__git' => 'git@github.com:getmim/content-pricing.git',
     '__license' => 'MIT',
     '__author' => [
--- modules/content-pricing/model/Pricing.php
+++ modules/content-pricing/model/Pricing.php
@@ -15,4 +15,59 @@ class Pricing extends \Mim\Model
     protected static $chains = [];
 
     protected static $q = [];
+
+    static function countUnpriced(string $type, object $conf, int $user=null): int{
+        $o_model = $conf->model;
+        $o_table = $o_model::getTable();
+        $o_f_id  = $conf->fields->id;
+        $o_f_user= $conf->fields->user;
+
+        $p_table = self::getTable();
+
+        $s_type  = self::escape($type);
+
+        $sql   = "
+            SELECT COUNT(*) `total`
+            FROM `$o_table` `o`
+                LEFT JOIN `$p_table` `p`
+                    ON `o`.`$o_f_id` = `p`.`object`
+                        AND `p`.`type` = '$s_type'
+            WHERE `p`.`id` IS NULL";
+
+        if($user)
+            $sql.= " AND `o`.`$o_f_user` = '$user'";
+
+        $result = self::query($sql, 'read');
+
+        return $result[0]->total ?? 0;
+    }
+
+    static function getUnpriced(string $type, object $conf, int $user=null, int $rpp=12, int $page=1): array{
+        $o_model = $conf->model;
+        $o_table = $o_model::getTable();
+        $o_f_id  = $conf->fields->id;
+        $o_f_user= $conf->fields->user;
+
+        $p_table = self::getTable();
+
+        $s_type  = self::escape($type);
+
+        $sql   = "
+            SELECT `o`.*
+            FROM `$o_table` `o`
+                LEFT JOIN `$p_table` `p`
+                    ON `o`.`$o_f_id` = `p`.`object`
+                        AND `p`.`type` = '$s_type'
+            WHERE `p`.`id` IS NULL";
+
+        if($user)
+            $sql.= " AND `o`.`$o_f_user` = '$user'";
+
+        $sql.= " ORDER BY `o`.`$o_f_id` DESC LIMIT ";
+        if($page>1)
+            $sql.= $page-1;
+        $sql.= $rpp;
+
+        return self::query($sql, 'read') ?? [];
+    }
 }
